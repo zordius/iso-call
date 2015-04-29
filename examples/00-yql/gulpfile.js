ee58be6acfadeca74794e00d92779da75838fd0f
Zordius Chen <zordius@yahoo-inc.com>
2015-04-29 11:16:47 +0800

    use default watch from nodemon

--- examples/00-yql/gulpfile.js
+++ examples/00-yql/gulpfile.js
@@ -2,28 +2,13 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var nodemon = require('nodemon');
 
-var restart_nodemon = function () {
-    setTimeout(function () {
-        nodemon.emit('restart');
-    }, 200);
-};
-
-gulp.task('watch_js', function () {
-    gulp.watch(['server.js', 'yql.js', 'yalconsle.js'], ['restart_nodemon']);
-});
-
-gulp.task('restart_nodemon', function () {
-    restart_nodemon();
-});
-
+// Start nodemon server
 gulp.task('nodemon_server', function () {
     nodemon({
-        ignore: '*',
         script: 'server.js',
-        ext: 'do_not_watch'
     }).on('log', function (log) {
         gutil.log(log.colour);
     });
 });
 
-gulp.task('default', ['watch_js', 'nodemon_server']);
+gulp.task('default', ['nodemon_server']);
